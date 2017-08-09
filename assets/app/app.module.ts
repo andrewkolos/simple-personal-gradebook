202a77ed2804dd72c3ca3303c47ba4df8d4947a5
andrewkolos <andrewrkolos@gmail.com>
2017-08-08 21:36:27 -0400

    move Assignment component
    Change the component folder structure so that child components (e.g. Assignment) exist in a directory within the directory of the parent component (e.g. Category).

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
-import {AssignmentComponent} from "./assignments/assignment.component";
+import {AssignmentComponent} from "./gradebook/category/assignment/assignment.component"
 
 @NgModule({
     declarations: [
