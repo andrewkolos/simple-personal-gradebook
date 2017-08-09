2b0ac1825fea8fe8ba95f6c9da5c80de5e54e0df
andrewkolos <andrewrkolos@gmail.com>
2017-08-08 21:09:37 -0400

    add the Assignment front-end model.

---
+++ assets/app/assignments/assignment.model.ts
@@ -0,0 +1,18 @@
+export class Assignment {
+    /** A string uniquely identifying this assignment. */
+    private _id: string;
+
+    /**
+     * @param {string} id - The unique identifier for this assignment object.
+     * @param {string} name - The name of the assignment.
+     * @param {number} earned - The number of points/marks earned by the student for this assignment.
+     * @param {number} worth - The number of points/marks this assignment is worth.
+     */
+    constructor(id: string, public name: string, public earned?: number, public worth?: number) {
+        this._id = id;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+}
