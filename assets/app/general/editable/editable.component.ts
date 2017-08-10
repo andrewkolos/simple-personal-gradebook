04dcfbd8641983b8f03970ee84f0b41ec64967b7
andrewkolos <andrewrkolos@gmail.com>
2017-08-10 00:19:24 -0400

    modify editable's behavior
    Changes the editable component's behavior so that the value will be saved whenever the input loses focus except via the escape key.

--- assets/app/general/editable/editable.component.ts
+++ assets/app/general/editable/editable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, OnChanges, ViewChild, ElementRef, Input, Output, EventEmitter} from '@angular/core';
 
 
 export enum EditMode {
@@ -9,18 +9,21 @@ export enum EditMode {
 @Component({
     selector: 'app-editable',
     template: `
-        <span *ngIf="currentMode === EditModeType.plainText" (click)="currentMode = EditModeType.input">{{ value }}</span>
+        <span *ngIf="currentMode === EditModeType.plainText"
+              (click)="tempValue = value; currentMode = EditModeType.input">{{ value }}</span>
 
-        <input appAutofocus *ngIf="currentMode === EditModeType.input" type="text" [value]="value" (keyup.enter)="saveValue($event.target.value)"
-               (blur)="currentMode = EditModeType.plainText" />
+        <input appAutofocus *ngIf="currentMode === EditModeType.input" type="text" [value]="value"
+               (blur)="saveValue($event.target.value); ignoreChanges = false"
+               (keyup.escape)="ignoreChanges = true; currentMode = EditModeType.plainText"/>
     `
 })
 export class EditableComponent {
 
-    EditModeType : any = EditMode;
+    EditModeType: any = EditMode;
 
     currentMode: EditMode;
     @Input() value: string; // the current value of this editable
+    ignoreChanges: boolean;
     @Output() onValueChanged = new EventEmitter<string>();
 
     // sets default mode and default value if not defined by input
@@ -29,6 +32,8 @@ export class EditableComponent {
 
         if (this.value === "" || this.value === undefined)
             this.value = "--";
+
+        this.ignoreChanges = false;
     }
 
     saveValue(text: string) {
