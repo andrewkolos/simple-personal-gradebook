aa11387666dab24966aec97e176250e70dcb45d1
andrewkolos <andrewrkolos@gmail.com>
2017-08-15 12:04:47 -0400

    add id property to front-end category model

--- assets/app/gradebook/category/category.model.ts
+++ assets/app/gradebook/category/category.model.ts
@@ -1,7 +1,21 @@
 import {Assignment} from "./assignment/assignment.model";
 
 export class Category {
+    /** A string uniquely identifying this category. */
+    private _id: string;
 
-    constructor(public name: string, public assignments: Assignment[], public weight?: number) {}
+    /**
+     *
+     * @param {string} id - The unique identifier of this category object.
+     * @param {string} name - The name of the category.
+     * @param {Assignment[]} assignments - The assignments that belong to this category.
+     * @param {number} weight - The percentage of the course grade this category is worth.
+     */
+    constructor(id: string, public name: string, public assignments: Assignment[], public weight?: number) {
+        this._id = id;
+    }
 
+    get id(): string {
+        return this._id;
+    }
 }
