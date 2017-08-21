b8870b0595df62b749743446dd8849189fa0a658
andrewkolos <andrewrkolos@gmail.com>
2017-08-20 23:23:58 -0400

    add weight input to category component

--- assets/app/gradebook/category/category.component.ts
+++ assets/app/gradebook/category/category.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {Assignment} from "./assignment/assignment.model";
 import {Category} from "./category.model";
+import {InputRejectingFunctions} from "../editable/editable.component";
 
 @Component({
     selector: 'app-category',
@@ -8,7 +8,16 @@ import {Category} from "./category.model";
         <div class="col-12">
             <div class="row">
                 <div class="col-5">{{name}}</div>
-                <div class="col-3 offset-2">Worth:</div>
+                <div class="col-3 offset-2">
+                    <div class="row">
+                        <div class="col-6 text-right"> Worth:</div>
+                        <div class="col-6">
+                            <app-editable [maxLength]="3" [(value)]="category.weight"
+                                          [inputRejectingValidators]="weightInputRejectingValidators"></app-editable>
+                            %
+                        </div>
+                    </div>
+                </div>
                 <div class="col-2"></div>
             </div>
         </div>
@@ -22,5 +31,10 @@ import {Category} from "./category.model";
 export class CategoryComponent {
 
     @Input() category: Category;
+
+    weightInputRejectingValidators = [{fn: InputRejectingFunctions.numeric, feedback: "Must be numeric."},
+        {fn: InputRejectingFunctions.nonempty, feedback: "Cannot be empty."},
+        {fn: InputRejectingFunctions.max(100), feedback: "Cannot exceed 100%."},
+        {fn: InputRejectingFunctions.min(0), feedback: "Cannot be negative."}];
 
 }
