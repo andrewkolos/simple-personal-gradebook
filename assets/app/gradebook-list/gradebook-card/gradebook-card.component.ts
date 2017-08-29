e5f39f5d469e92041f402c4ffb304751d9df2f55
andrewkolos <andrewrkolos@gmail.com>
2017-08-29 12:26:44 -0400

    add base for gradebook lists and cards

--- assets/app/gradebook-list/gradebook-card/gradebook-card.component.ts
+++ assets/app/gradebook-list/gradebook-card/gradebook-card.component.ts
@@ -1,10 +1,29 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {Gradebook} from "../../gradebook/gradebook.model";
 
 @Component({
     selector: 'app-gradebook-card',
     template: `
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title"><a>{{gradebook.name}}</a></h4>
+
+                <p class="card-text">Hello, I am some placeholder text.</p>
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="moreActionsDropdown"
+                            data-toggle="dropdown">
+                        More actions
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Change name</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item text-danger" href="#">Delete</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     `
 })
 export class GradebookCardComponent {
-    
+    @Input() gradebook: Gradebook;
 }
--- assets/app/gradebook-list/gradebook-list.component.ts
+++ assets/app/gradebook-list/gradebook-list.component.ts
@@ -1,11 +1,29 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Gradebook} from "../gradebook/gradebook.model";
+import {GradebookService} from "../gradebook/gradebook.service";
 
 @Component({
     selector: 'app-gradebook-list',
     template: `
-        
+        <div class="card-deck">
+            <!-- May need to add card class and remove it from gradebook card component -->
+            <app-gradebook-card *ngFor="let gradebook of gradebooks" [gradebook]="gradebook"></app-gradebook-card>
+        </div>
     `
 })
-export class GradebookListComponent {
+export class GradebookListComponent implements OnInit {
 
+    constructor(private gradebookService: GradebookService) {
+    }
+
+    ngOnInit() {
+        this.gradebookService.getGradebooks().subscribe(
+            (gradebooks: Gradebook[]) => {
+                console.log(gradebooks);
+                this.gradebooks = gradebooks;
+            }
+        );
+    }
+
+    gradebooks: Gradebook[];
 }
