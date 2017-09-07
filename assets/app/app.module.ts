ccc6315c50b4d647e03f472561ba83cea5eb962a
andrewkolos <andrewrkolos@gmail.com>
2017-09-07 04:04:00 -0400

    add auth module with lazy loading

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -8,9 +8,6 @@ import {HttpModule} from "@angular/http";
 import {HeaderComponent} from "./header.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
 import {routing} from "./app.routing";
-import {SigninComponent} from "./auth/signin.component";
-import {SignupComponent} from "./auth/signup.component";
-import {LogoutComponent} from "./auth/logout.component";
 import {AuthService} from "./auth/auth.service";
 import {GradebookModule} from "./gradebook/gradebook.module";
 
@@ -19,13 +16,14 @@ import {GradebookModule} from "./gradebook/gradebook.module";
         AppComponent,
         AutofocusDirective,
         HeaderComponent,
-        AuthenticationComponent,
-        SigninComponent,
-        SignupComponent,
-        LogoutComponent
+        AuthenticationComponent
     ],
     providers: [AuthService],
-    imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing, GradebookModule],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        routing,
+        GradebookModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,14 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {GradebookListComponent} from "./gradebook-list/gradebook-list.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
-import {AUTH_ROUTES} from "./auth/auth.routes";
 import {GradebookComponent} from "./gradebook/gradebook.component";
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/auth/signin', pathMatch: 'full' },
     { path: 'gradebook-list', component: GradebookListComponent },
     { path: 'gradebook/:id', component: GradebookComponent },
-    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
+    { path: 'auth', component: AuthenticationComponent, loadChildren: './auth/auth.module#AuthModule' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
